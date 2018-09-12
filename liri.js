11d94d869a394d531eb4d5682409e2e72bc0ef1f
Jd0h Norteman <jnorteman91@gmail.com>
2018-09-12 06:33:13 -0400

    final corrections

--- liri.js
+++ liri.js
@@ -1,33 +1,12 @@
 require("dotenv").config();
 var keys = require("./keysfolder/keys.js");
 var request = require("request");
+var fs = require("fs");
 var Spotify = require("node-spotify-api");
-var Spotify = new Spotify(keys.spotify);
-
-var action = process.argv[2];
-var parameter = process.argv[3];
-
-function switchCase() {
-    switch(action) {
-        case "concert-this":
-            bandsInTown(parameter);
-            break;
-        case "spotify-this-song":
-            spotifySong(parameter);
-            break;
-        case "movie-this":
-            omdbInfo(parameter);
-            break;
-        case "do-what-it-says":
-            getRandom();
-            break;
-
-            default:
-            log("Error: Invalid Search");
-            break;
-    }
-};
-
+var spotify = new Spotify(keys.spotify);
+// var getArtistNames = function(artist) {
+//     return artist.name;
+// }
 function bandsintown(parameter){
 
     if (action === "concert-this"){
@@ -52,17 +31,17 @@ function bandsintown(parameter){
                 var day = time.substring(8,10);
                 var form = month + "/" + day + "/" + year;
 
-                log("\n-------------------------------\n");
+                console.log("\n-------------------------------\n");
 
-                log("Date: " + form);
-                log("Name: " + js[i].venue.name);
-                log("City: " + js[i].venue.city);
+                console.log("Date: " + form);
+                console.log("Name: " + js[i].venue.name);
+                console.log("City: " + js[i].venue.city);
 
                 if (js[i].venue.region !== "") {
-                    log("Country: " + js[i].venue.region);
+                    console.log("Country: " + js[i].venue.region);
                 }
-                log("Country: " + js[i].venue.country);
-                log("\n------------------------------\n");
+                console.log("Country: " + js[i].venue.country);
+                console.log("\n------------------------------\n");
             }
         }
     });
@@ -70,29 +49,27 @@ function bandsintown(parameter){
 
 function spotifySong(parameter) {
     var searchSong;
-    if  (parameter === undeifined) {
+    if  (parameter === undefined) {
         searchSong = "The Sign Ace of Base";
 
-    } else {
-        searchSong = parameter;
-    }
+    } 
 
-    spotifySong.search({
+    spotify.search({
         type: "track",
         query: searchSong
     },
     function(err, data) {
         if (err) {
-            log("An error has occured: " + err);
+            console.log("An error has occured: " + err);
             return;
-        } else {
-            log("\n------------------------------\n");
-            log("Artist: " + data.tracks.items[0].artist[0].name);
-            log("Song: " + data.tracks.items[0].name);
-            log("Preview: " + data.tracks.items[3].preview_url);
-            log("Album: " + data.tracks.items[0].album.name);
-            log("\n------------------------------\n");
-        }
+        } 
+            console.log("\n------------------------------\n");
+            console.log("Artist: " + data.tracks.items[0].artist[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Preview: " + data.tracks.items[3].preview_url);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("\n------------------------------\n");
+        
     });
 };
 
@@ -110,16 +87,16 @@ function movieInfo(parameter) {
     request(queryURL,function(err, res, body) {
         var bodyOf = JSON.parse(body);
         if (!err && res.statusCode === 200) {
-            log("\n-----------------------------\n");
-            log("Title: " + bodyOf.Title);
-            log("Release Year: " + bodyOf.Year);
-            log("IMDB Rating: " + bodyOf.IMDBRating);
-            log("Rotten Tomatoes Rating: " + bodyOf.Ratings[1].Value);
-            log("Country: " + bodyOf.Country);
-            log("Language: " + bodyOf.Language);
-            log("Plot: " + bodyOf.Plot);
-            log("Actors: " + bodyOf.Actors);
-            log("\n-----------------------------\n");
+            console.log("\n-----------------------------\n");
+            console.log("Title: " + bodyOf.Title);
+            console.log("Release Year: " + bodyOf.Year);
+            console.log("IMDB Rating: " + bodyOf.IMDBRating);
+            console.log("Rotten Tomatoes Rating: " + bodyOf.Ratings[1].Value);
+            console.log("Country: " + bodyOf.Country);
+            console.log("Language: " + bodyOf.Language);
+            console.log("Plot: " + bodyOf.Plot);
+            console.log("Actors: " + bodyOf.Actors);
+            console.log("\n-----------------------------\n");
         }
     });
 };
@@ -128,15 +105,15 @@ function getRandom() {
     fs.readfile("random.txt", "utf8", function(err, data) {
 
         if (err) {
-            return log(err);
+            return console.log(err);
         }
 
         var dataArray = data.split(",");
 
         if (dataArray[0] === "spotify-this-song") {
-            var  songCheck = dataArray[1].trim()slice(1, -1);
+            var  songCheck = dataArray[1].trim().slice(1, -1);
             spotifySong(songCheck);
-        } else if (dataArray[0] === "concert-this" {
+        } else if (dataArray[0] === "concert-this") {
             if (dataArray[1].charAt(1) === "") {
                 var dataLength = dataArray[1].length -1;
                 var data = dataArray[1].substring(2, dataLength);
@@ -144,7 +121,6 @@ function getRandom() {
                 bandsintown(data);
             } else {
                 var bandName = dataArray[1].trim();
-                console.log(bandName);
                 console.log(bandName);
             }
         } else if(dataArray[0] === "movie-this") {
@@ -155,6 +131,7 @@ function getRandom() {
 };
 
 function log(dataLog) {
+    
     console.log(dataLog);
 
     fs.appendFile("log.txt", dataLog + "\n", function(err) {
@@ -163,4 +140,29 @@ function log(dataLog) {
     });
 }
 
-switchCase();
+var select = function(action, parameter) {
+    switch (action) {
+        case "concert-this":
+            bandsInTown(parameter);
+            break;
+        case "spotify-this-song":
+            spotifySong(parameter);
+            break;
+        case "movie-this":
+            omdbInfo(parameter);
+            break;
+        case "do-what-it-says":
+            getRandom();
+            break;
+
+            default:
+            log("Error: Invalid Search");
+            break;
+    }
+};
+
+var runLiri = function(argumentone, argumenttwo) {
+    select(argumentone, argumenttwo);
+}
+
+runLiri(process.argv[2], process.argv.slice(3).join(" "));
